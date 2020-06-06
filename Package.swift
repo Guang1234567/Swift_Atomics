aef73e76a7a5595c1c468324ed3f2e3bba9b619f
GuangGuang <lihansey163@163.com>
2020-06-07 00:36:09 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,31 +1,18 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 
 import PackageDescription
 
-#if swift(>=4.0)
-
 let package = Package(
-  name: "SwiftAtomics",
-  products: [
-    .library(name: "SwiftAtomics", type: .static, targets: ["SwiftAtomics"]),
-    .library(name: "CAtomics", type: .static, targets: ["CAtomics"]),
-  ],
-  targets: [
-    .target(name: "SwiftAtomics", dependencies: ["CAtomics"]),
-    .testTarget(name: "SwiftAtomicsTests", dependencies: ["SwiftAtomics"]),
-    .target(name: "CAtomics", dependencies: []),
-    .testTarget(name: "CAtomicsTests", dependencies: ["CAtomics"]),
-  ],
-  swiftLanguageVersions: [.v3, .v4, .v4_2, .version("5")]
+        name: "SwiftAtomics",
+        products: [
+            .library(name: "SwiftAtomics", type: .dynamic, targets: ["SwiftAtomics"]),
+            .library(name: "CAtomics", type: .dynamic, targets: ["CAtomics"]),
+        ],
+        targets: [
+            .target(name: "SwiftAtomics", dependencies: ["CAtomics"]),
+            .testTarget(name: "SwiftAtomicsTests", dependencies: ["SwiftAtomics"]),
+            .target(name: "CAtomics", dependencies: []),
+            .testTarget(name: "CAtomicsTests", dependencies: ["CAtomics"]),
+        ]/*,
+  swiftLanguageVersions: [.v4, .v4_2, .v5]*/
 )
-
-#else
-
-let package = Package(
-  name: "SwiftAtomics",
-  targets: [
-    Target(name: "SwiftAtomics", dependencies: ["CAtomics"]),
-  ]
-)
-
-#endif
